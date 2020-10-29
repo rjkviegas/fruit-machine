346aa680f4997daf3a7a1ad97af218c22f473391
Ryan Viegas <rjkviegas@gmail.com>
2020-10-29 21:49:41 +0000

    Refactor inline slot_key variable

--- lib/play_fruit_machine.py
+++ lib/play_fruit_machine.py
@@ -2,10 +2,9 @@ from random import randint
 
 def play_fruit_machine():
     slots_dict = { 0: 'black', 1: 'white' }
-    slot_key = randint(0, 1)
     return [
-        slots_dict[slot_key],
-        slots_dict[slot_key],
-        slots_dict[slot_key],
-        slots_dict[slot_key]
+        slots_dict[randint(0, 1)],
+        slots_dict[randint(0, 1)],
+        slots_dict[randint(0, 1)],
+        slots_dict[randint(0, 1)]
     ]
