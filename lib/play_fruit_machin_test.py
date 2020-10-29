2c3f114141dfcd3b28baacb800ba2768899fd577
Ryan Viegas <rjkviegas@gmail.com>
2020-10-29 21:47:53 +0000

    Test play_fruit_machine returns 4 white

--- lib/play_fruit_machin_test.py
+++ lib/play_fruit_machin_test.py
@@ -1,9 +1,20 @@
 from play_fruit_machine import play_fruit_machine
+from unittest import mock
 
-def test_play_fruit_machine():
+@mock.patch("play_fruit_machine.randint", return_value=0, autospec=True)
+def test_play_fruit_machine_returns_4_black(mock_randint):
     assert play_fruit_machine() == [
         'black',
         'black',
         'black',
         'black'
+    ]
+
+@mock.patch("play_fruit_machine.randint", return_value=1, autospec=True)
+def test_play_fruit_machine_returns_4_white(mock_randint):
+    assert play_fruit_machine() == [
+        'white',
+        'white',
+        'white',
+        'white'
     ]
--- lib/play_fruit_machine.py
+++ lib/play_fruit_machine.py
@@ -1,8 +1,11 @@
+from random import randint
+
 def play_fruit_machine():
-    slots_dict = { 0: 'black' }
+    slots_dict = { 0: 'black', 1: 'white' }
+    slot_key = randint(0, 1)
     return [
-        slots_dict[0],
-        slots_dict[0],
-        slots_dict[0],
-        slots_dict[0]
+        slots_dict[slot_key],
+        slots_dict[slot_key],
+        slots_dict[slot_key],
+        slots_dict[slot_key]
     ]
