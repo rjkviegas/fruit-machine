951b4f27e1b794be5c2422ed4e381defafbfd499
Ryan Viegas <rjkviegas@gmail.com>
2020-11-02 21:18:36 +0000

    Refactor get_balance in Player

--- lib/Player.py
+++ lib/Player.py
@@ -7,6 +7,9 @@ class Player:
     def __init__(self, balance):
         self.balance = balance
     
+    def get_balance(self):
+        return self.balance
+    
     def play(self, game):
         self.pay_for(game)
         if game.is_winner(game.play()):
@@ -14,7 +17,7 @@ class Player:
             game.balance = 0
     
     def pay_for(self, game):
-        if self.balance < game.fee:
+        if self.get_balance() < game.fee:
             raise InsufficientBalance('Insufficient balance to play')
         self.balance -= game.fee
         game.balance += game.fee
